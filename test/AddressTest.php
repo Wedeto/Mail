533e6cf0a34485cd406dd1399891a9184358f86f
Egbert van der Wal <ewal@pointpro.nl>
2017-05-17 09:22:18 +0200

    Imported tests from zend mail and zend mime - need to adapt

---
+++ test/AddressTest.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\Mail;
+
+use Zend\Mail\Address;
+
+/**
+ * @covers Zend\Mail\Address<extended>
+ */
+class AddressTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDoesNotRequireNameForInstantiation()
+    {
+        $address = new Address('zf-devteam@zend.com');
+        $this->assertEquals('zf-devteam@zend.com', $address->getEmail());
+        $this->assertNull($address->getName());
+    }
+
+    public function testAcceptsNameViaConstructor()
+    {
+        $address = new Address('zf-devteam@zend.com', 'ZF DevTeam');
+        $this->assertEquals('zf-devteam@zend.com', $address->getEmail());
+        $this->assertEquals('ZF DevTeam', $address->getName());
+    }
+
+    public function testToStringCreatesStringRepresentation()
+    {
+        $address = new Address('zf-devteam@zend.com', 'ZF DevTeam');
+        $this->assertEquals('ZF DevTeam <zf-devteam@zend.com>', $address->toString());
+    }
+
+    /**
+     * @dataProvider invalidSenderDataProvider
+     * @group ZF2015-04
+     * @param string $email
+     * @param null|string $name
+     */
+    public function testSetAddressInvalidAddressObject($email, $name)
+    {
+        $this->setExpectedException('Zend\Mail\Exception\InvalidArgumentException');
+        new Address($email, $name);
+    }
+
+    public function invalidSenderDataProvider()
+    {
+        return [
+            // Description => [sender address, sender name],
+            'Empty' => ['', null],
+            'any ASCII' => ['azAZ09-_', null],
+            'any UTF-8' => ['ázÁZ09-_', null],
+
+            // CRLF @group ZF2015-04 cases
+            ["foo@bar\n", null],
+            ["foo@bar\r", null],
+            ["foo@bar\r\n", null],
+            ["foo@bar", "\r"],
+            ["foo@bar", "\n"],
+            ["foo@bar", "\r\n"],
+            ["foo@bar", "foo\r\nevilBody"],
+            ["foo@bar", "\r\nevilBody"],
+        ];
+    }
+
+    /**
+     * @dataProvider validSenderDataProvider
+     * @param string $email
+     * @param null|string $name
+     */
+    public function testSetAddressValidAddressObject($email, $name)
+    {
+        $address = new Address($email, $name);
+        $this->assertInstanceOf('\Zend\Mail\Address', $address);
+    }
+
+    public function validSenderDataProvider()
+    {
+        return [
+            // Description => [sender address, sender name],
+            'german IDN' => ['oau@ä-umlaut.de', null],
+        ];
+    }
+}
---
+++ test/HeaderWrapTest.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\Mail\Header;
+
+use Zend\Mail\Header\GenericHeader;
+use Zend\Mail\Header\HeaderWrap;
+
+/**
+ * @group      Zend_Mail
+ * @covers Zend\Mail\Header\HeaderWrap<extended>
+ */
+class HeaderWrapTest extends \PHPUnit_Framework_TestCase
+{
+    public function testWrapUnstructuredHeaderAscii()
+    {
+        $string = str_repeat('foobarblahblahblah baz bat', 4);
+        $header = $this->getMock('Zend\Mail\Header\UnstructuredInterface');
+        $header->expects($this->any())
+            ->method('getEncoding')
+            ->will($this->returnValue('ASCII'));
+        $expected = wordwrap($string, 78, "\r\n ");
+
+        $test = HeaderWrap::wrap($string, $header);
+        $this->assertEquals($expected, $test);
+    }
+
+    /**
+     * @group ZF2-258
+     */
+    public function testWrapUnstructuredHeaderMime()
+    {
+        $string = str_repeat('foobarblahblahblah baz bat', 3);
+        $header = $this->getMock('Zend\Mail\Header\UnstructuredInterface');
+        $header->expects($this->any())
+            ->method('getEncoding')
+            ->will($this->returnValue('UTF-8'));
+        $expected = "=?UTF-8?Q?foobarblahblahblah=20baz=20batfoobarblahblahblah=20baz=20?=\r\n"
+                    . " =?UTF-8?Q?batfoobarblahblahblah=20baz=20bat?=";
+
+        $test = HeaderWrap::wrap($string, $header);
+        $this->assertEquals($expected, $test);
+        $this->assertEquals($string, iconv_mime_decode($test, ICONV_MIME_DECODE_CONTINUE_ON_ERROR, 'UTF-8'));
+    }
+
+    /**
+     * @group ZF2-359
+     */
+    public function testMimeEncoding()
+    {
+        $string   = 'Umlauts: ä';
+        $expected = '=?UTF-8?Q?Umlauts:=20=C3=A4?=';
+
+        $test = HeaderWrap::mimeEncodeValue($string, 'UTF-8', 78);
+        $this->assertEquals($expected, $test);
+        $this->assertEquals($string, iconv_mime_decode($test, ICONV_MIME_DECODE_CONTINUE_ON_ERROR, 'UTF-8'));
+    }
+
+    public function testMimeDecoding()
+    {
+        $expected = str_repeat('foobarblahblahblah baz bat', 3);
+        $encoded = "=?UTF-8?Q?foobarblahblahblah=20baz=20batfoobarblahblahblah=20baz=20?=\r\n"
+                    . " =?UTF-8?Q?batfoobarblahblahblah=20baz=20bat?=";
+
+        $decoded = HeaderWrap::mimeDecodeValue($encoded);
+
+        $this->assertEquals($expected, $decoded);
+    }
+
+    /**
+     * Test that fails with HeaderWrap::canBeEncoded at lowest level:
+     *   iconv_mime_encode(): Unknown error (7)
+     *
+     * which can be triggered as:
+     *   $header = new GenericHeader($name, $value);
+     */
+    public function testCanBeEncoded()
+    {
+        // @codingStandardsIgnoreStart
+        $name    = 'Subject';
+        $value   = "[#77675] New Issue:xxxxxxxxx xxxxxxx xxxxxxxx xxxxxxxxxxxxx xxxxxxxxxx xxxxxxxx, tähtaeg xx.xx, xxxx";
+        $encoded = "Subject: =?UTF-8?Q?[#77675]=20New=20Issue:xxxxxxxxx=20xxxxxxx=20xxxxxxxx=20?=\r\n =?UTF-8?Q?xxxxxxxxxxxxx=20xxxxxxxxxx=20xxxxxxxx,=20t=C3=A4htaeg=20xx.xx,=20xxxx?=";
+        // @codingStandardsIgnoreEnd
+        //
+        $res = HeaderWrap::canBeEncoded($value);
+        $this->assertTrue($res);
+
+        $header = new GenericHeader($name, $value);
+        $res = $header->toString();
+        $this->assertEquals($encoded, $res);
+    }
+}
